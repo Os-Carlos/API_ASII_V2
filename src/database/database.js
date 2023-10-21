d520209f0cc1bb4985390bebe8acac91dfd58beb
carlos osorio <cordonezo4@miumg.edu.gt>
2023-10-21 14:55:22 -0600

    db sync

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ import Sequelize from "sequelize";
 
 export const sequelize = new Sequelize(
     "postgres", // db name,
-    "root", // username
+    "postgres", // username
     "omUeIQNvs2nkrmzf", // password
     {
         host: "db.zgcgkizkfhatmwkijhpw.supabase.co",
